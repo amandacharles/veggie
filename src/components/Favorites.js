d911ecab26adf70fa547d74435f489a47f697e7e
Amanda Charles <ancharles11@gmail.com>
2017-03-19 16:49:25 -0700

    map pins are working now

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,7 +1,44 @@
 import React, { Component, PropTypes } from 'react';
-import { Text, NavigatorIOS, View, TouchableHighlight, ListView } from 'react-native';
-import { Button, Card, CardSection, Input, Spinner } from './common';
+import { Text, NavigatorIOS, View, TouchableHighlight, ListView, Image, ScrollView} from 'react-native';
+import { Button, CardSection, Input, Spinner, Card, Clickable } from './common';
 import * as firebase from 'firebase';
+
+const styles = {
+    headerContentStyle: {
+      flexDirection: 'column',
+      justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+      fontSize: 18
+    },
+    thumbNailStyle: {
+      height: 50,
+      width: 50,
+      flexDirection: 'column',
+      justifyContent: 'flex-end'
+    },
+    thumbNailContainerStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginLeft: 10,
+      marginRight: 10,
+      padding: 1
+
+    },
+    imageStyle: {
+      height: 300,
+      flex: 1,
+      width: null
+    },
+  }
+
+  const { thumbNailStyle,
+    headerContentStyle,
+    thumbNailContainerStyle,
+    headerTextStyle,
+    imageStyle
+    } = styles;
+
 
 class Favorites extends Component {
   constructor(props){
@@ -44,8 +81,6 @@ class Favorites extends Component {
 //   }
 
 showFavorites(){
-    console.log(this.state.favorites.length);
-
     const favorites = this.state.favorites;
 
   Object.keys(favorites).map((key, index) => {
@@ -54,15 +89,51 @@ showFavorites(){
     return
     <View>
     <Text>{favorite.name}</Text>
-  </View>
+    </View>
   });
 }
 
 renderFavorite(fav) {
-  console.log(fav);
-  return <View>
-    <Text>{fav.name}</Text>
-  </View>
+  return (
+  <View>
+    <Card key={fav.name}>
+
+          <Clickable onPress={()=> Linking.openURL(fave.website)}>
+            <View style={thumbNailContainerStyle}>
+              <View >
+                <Text style={headerTextStyle}>{ fav.name }</Text>
+              </View>
+            </View>
+          </Clickable>
+
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+
+            <View style={{flexDirection:'row', justifyContent: 'flex-start'}}>
+                <CardSection>
+                  <View style={headerContentStyle}>
+                    <Clickable onPress={()=> Linking.openURL(fav.website)}>
+                    <Text>{fav.phone}</Text>
+                    <Text style={{flexWrap: "wrap"}}>{fav.veg_level_description}</Text>
+                    <Text>{fav.price_range}</Text>
+                    <Text style={{flexWrap: "wrap"}}>{fav.short_description}</Text>
+                  </Clickable>
+                  </View>
+                </CardSection>
+            </View>
+
+    <View style={{flexDirection:"column", justifyContent: "flex-end"}}>
+            <Clickable  style={{ flexDirection: 'column', justifyContent: 'flex-end'}}>
+            <View style={{ flexDirection: 'column', justifyContent: 'flex-end'}}>
+                <Image
+                  style={thumbNailStyle}
+                  source={require('./carrot.jpg')}/>
+              </View>
+            </Clickable>
+          </View>
+            </View>
+          </Card>
+        </View>
+      )
 }
 
 
@@ -70,9 +141,11 @@ renderFavorite(fav) {
     console.log(this.state.favorites);
     return (
       <View style={{flex: 1, justifyContent: 'center', borderWidth: 5}}>
+      <ScrollView>
           {/* {this.showFavorites()} */}
           {this.state.favorites.map(this.renderFavorite)}
-      </View>
+      </ScrollView>
+    </View>
     );
   }
 }
--- src/components/TheMap.js
+++ src/components/TheMap.js
@@ -22,19 +22,17 @@ class TheMap extends Component {
             latitude: 37.762033,
             longitude: -122.434759
         },
-        title: 'placeholder',
+        title: 'Placeholder',
         description: 'description'
       }
     ],
-    yelpResults: []
   }
 
   this.onRegionChange = this.onRegionChange.bind(this);
   this.componentWillMount = this.componentWillMount.bind(this)
-  this.callMyMarkers = this.callMyMarkers.bind(this);
+  // this.callMyMarkers = this.callMyMarkers.bind(this);
   this.callApi = this.callApi.bind(this);
-  this.createMarkers = this.createMarkers.bind(this);
-  // this.componentDidMount = this.componentDidMount.bind(this);
+  this.componentDidMount = this.componentDidMount.bind(this);
 
 }
 
@@ -48,83 +46,63 @@ componentWillMount(){
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           },
-           markers: [
-             {
-               latlng: {
-                   latitude: position.coords.latitude,
-                   longitude: position.coords.longitude
-               },
-               title: 'NEw Title!',
-               description: 'new description'
-             }
-           ]
+          markers: [
+            {
+              latlng: {
+                  latitude: 37.762033,
+                  longitude: -122.434759
+              },
+              title: 'Placeholder',
+              description: 'description'
+            }
+          ],
         })
       }
   )
   this.callApi()
-  this.createMarkers()
 }
 
-// componentDidMount(){
-//   this.createMarkers()
-// }
+componentDidMount(){
+  // this.callMyMarkers()
+}
 
 callApi(){
-  axios.get(`https://api.yelp.com/v3/businesses/search?categories=vegan,restaurants,vegetarian&latitude=${this.state.region.latitude}&longitude=${this.state.region.longitude}&limit=50&radius=30000`,
+  setResults = [];
+  axios.get(`http://www.vegguide.org/search/by-lat-long/${this.state.region.latitude},${this.state.region.longitude}/distance=5/filter/veg_level=5`,
     {headers: {
-    'Authorization': 'Bearer KaYmgMa-GXIlQcg3gmjwolPMnSFOkLa9dzaG5NDhk5l1G5LfekRfzCMyj6WeoEE2KSON7mHxCjDYcNZY62DHgLNuf7-ZTEYwm2QIusj0cBtmaU5-C_eBraZFbfDCWHYx'
+    'User-Agennt': 'Vegout Project'
   }
 })
-  .then((res) => {
-    let veggieArray = [];
-
-    for (let i = 0; i < res.data.businesses.length; i++) {
-      let categoriesArray = res.data.businesses[i].categories
-        categoriesArray.map(category => {
-          if(category.alias === 'vegetarian' ||
-          category.alias === 'vegan' ||
-          category.alias === 'thai' ||
-          category.alias === 'indian' ||
-          category.alias === 'mediterranean' ||
-          category.alias === 'asianfusion'
-        ){
-            veggieArray.push(res.data.businesses[i])
-          }
-        })
-       }
-       this.setState({ yelpResults: veggieArray})
-       this.createMarkers()
-       })
-       .catch(function(err){
-       console.log(err)
-      })
-  }
-
-
-createMarkers(){
-  let markersArray = [];
-  this.state.yelpResults.map(result => {
-    markersArray.push(
-      {
-        latlng: {
-            latitude: result.coordinates.latitude,
-            longitude: result.coordinates.longitude
-        },
-        title: result.name,
-        description: result.categories[0].title,
-        url: result.url
+.then((res) => {
+  res.data.entries.map(entry => {
+    axios.get(`https://api.yelp.com/v3/businesses/search?term=${entry.name}&latitude=${this.state.region.latitude}&longitude=${this.state.region.longitude}&limit=1&radius=30000`,
+        {headers: {
+        'Authorization': 'Bearer KaYmgMa-GXIlQcg3gmjwolPMnSFOkLa9dzaG5NDhk5l1G5LfekRfzCMyj6WeoEE2KSON7mHxCjDYcNZY62DHgLNuf7-ZTEYwm2QIusj0cBtmaU5-C_eBraZFbfDCWHYx'
       }
-
-    )
-      // console.log(markersArray);
-      this.setState({
-        markers: markersArray
-      })
-
+    })
+      .then((res) => {
+        if(res.data.businesses[0].coordinates.latitude && res.data.businesses[0].coordinates.longitude){
+        setResults.push({
+            latlng: {
+                latitude: res.data.businesses[0].coordinates.latitude,
+                longitude: res.data.businesses[0].coordinates.longitude
+            },
+            title: res.data.businesses[0].name,
+            description: res.data.businesses[0].categories[0].title
+          }
+        )}
+        this.setState({
+          markers: setResults
+        })
+           })
+           .catch(function(err){
+           console.log(err)
+          })
   })
-  console.log(this.state.markers);
-  // console.log(markersArray);
-  // console.log(this.state.yelpResults);
+})
+.catch(function(err){
+      console.log(err)
+  })
 }
 
 
@@ -133,19 +111,18 @@ onRegionChange(region) {
 }
 
 callMyMarkers(){
+  console.log(this.state.markers);
   this.state.markers.map(marker => {
-  <MapView.Marker
-    image={require('./carrot.jpg')}
-    style={{height: 30}}
+    return <MapView.Marker
+    key={marker.title}
     coordinate={marker.latlng}
-    title={marker.title}
     description={marker.description}
-  />
+    title={marker.title}>
+  </MapView.Marker>
 })
 }
 
   render() {
-    // console.log(this.state.markers);
     return (
       <View style={{flex: 1}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#F5FCFF'}}>
@@ -158,8 +135,8 @@ callMyMarkers(){
           onRegionChange={this.state.onRegionChange}>
           {this.state.markers.map(marker => {
             return <MapView.Marker
-              key={marker.title}
-            coordinate={marker['latlng']}
+            key={marker.title}
+            coordinate={marker.latlng}
             title={marker.title}
             description={marker.description}
           >
